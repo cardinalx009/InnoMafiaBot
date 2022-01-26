da4ac2e5bb9a8745b99600c690185f661662b7ba
Zener_085 <57841939+Zener085@users.noreply.github.com>
2022-01-26 23:52:22 +0300

    Change main module
    change telebot to aiogram

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
-import telebot
+from aiogram import Bot, Dispatcher, types, executor
+from users_info import *
 
-bot = telebot.TeleBot("5058417039:AAE8bbyEkehPSCLCDOcOC-z8aB75t5KEMQc")
+bot = Dispatcher(Bot("5058417039:AAE8bbyEkehPSCLCDOcOC-z8aB75t5KEMQc"))
 
 
 @bot.message_handler(content_types=['text'], commands=["start"])
-def get_text_messages(message):
+async def get_text_messages(message: types.Message):
     """start message, introduction to functions of bot"""
-    bot.reply_to(message, "Hello! I am Innopolis Mafia Bot.\n"
+    await message.reply("Hello! I am Innopolis Mafia Bot.\n"
                           "I can send you personal info about your games in this season or all time")
 
 
 @bot.message_handler(commands=["info_bot", "bot_info"])
-def get_info_about_bot(message):
+async def get_info_about_bot(message: types.Message):
     """global information about bot"""
-    bot.reply_to(message, "I am Innopolis Mafia Bot, I help host in game and collect some data about players.\n"
+    await message.reply("I am Innopolis Mafia Bot, I help host in game and collect some data about players.\n"
                           "You can get information about: game wins/loses, win rate, attendance and other.\n"
                           "For that you have to write your personal id or telegram alias.")
 
 
 @bot.message_handler(commands=["personal_info"])
-def send_personal_info(message):
+async def send_personal_info(message: types.Message):
     """write personal info to user"""
-    bot.reply_to(message, "")
+    await message.reply("")
 
 
-bot.infinity_polling()
+if __name__ == "__main__":
+    executor.start_polling(bot, skip_updates=True)
