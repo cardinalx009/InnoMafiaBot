44cff2ecd18f388917a221aef9946ae4f91dbdef
Zener_085 <57841939+Zener085@users.noreply.github.com>
2022-03-07 10:43:26 +0300

    Update sending rating and refactoring
    Refactor Pinka's code:
    Rename functions, add docs for these functions, change settings (send rating to group chat, not only privately), rewrite additional functions for importing and changing list of players to users.info
    
    Update rating:
    Send top 3 players, if some players have the same number of rating, they devided places with each others

--- main.py
+++ main.py
@@ -2,50 +2,31 @@
 # Catching messages to him and send some answers
 
 # Global task
-# TODO implement returning table for mafia
 # TODO implement some answers for pm messages
 
-# The nearest tasks
-# TODO share to mafia group this bot
-# TODO add image for bot
-
-import dotenv
-from os import getenv
 from aiogram import Bot, Dispatcher, types, executor
+
 from users_info import *
 
-dotenv.load_dotenv(dotenv.find_dotenv())
+load_dotenv(find_dotenv())
+
 bot = Bot(getenv("TOKEN"))
 dp = Dispatcher(bot)
 
 
 def get_markup() -> types.InlineKeyboardMarkup:
+    """Menu buttons, included main functions for bot"""
     keyboard = types.InlineKeyboardMarkup(resize_keyboard=True)
     keyboard.add(types.InlineKeyboardButton("Info", callback_data="info"))
-    keyboard.add(types.InlineKeyboardButton("My statistics", callback_data="stats"))
+    keyboard.add(types.InlineKeyboardButton("My statistics", callback_data="personal_info"))
     keyboard.add(types.InlineKeyboardButton("Top 3 people", callback_data="top"))
-    keyboard.add(types.InlineKeyboardButton("Group url", url="https://t.me/innopolismafiaclub"))
+    keyboard.add(types.InlineKeyboardButton("Group url", url="https://t.me/InnopolisMafia"))
     return keyboard
-
-
-def generate_top(top: int) -> str:
-    answer = "Top mafia people:\n"
-    for i, (alias, info) in enumerate(
-            sorted(parse_data(import_string()).items(), key=lambda p: p[1]['points'], reverse=True)[:top], start=1):
-        answer += f"{i}: {alias} have {info['points']} points\n"
-    return answer
-
-
-@dp.message_handler(lambda message: message.text == "/stat", content_types=['text'])
-async def send_statistic(message: types.Message):
-    """Top 3 statistics"""
-    top: int = 3
-    await message.answer(generate_top(top), reply_markup=(get_markup() if message.from_user.id == message.chat.id else None))
 
 
 @dp.message_handler(content_types=['text'])
 async def get_text_messages(message: types.Message):
-    """start message, introduction to functions of bot"""
+    """Start message, introduction to functions of bot"""
 
     await message.reply(
         "Hello! I am Innopolis Mafia Bot.\n"
@@ -55,8 +36,9 @@ async def get_text_messages(message: types.Message):
     )
 
 
-@dp.callback_query_handler(lambda c: c.data == "info")
-async def inline_info(callback_query: types.CallbackQuery):
+@dp.callback_query_handler(lambda c: c.data in ["info", "info_bot", "bot_info"])
+async def about(callback_query: types.CallbackQuery):
+    """Send info about bot"""
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(
         callback_query.from_user.id,
@@ -67,17 +49,18 @@ async def inline_info(callback_query: types.CallbackQuery):
     )
 
 
-@dp.callback_query_handler(lambda c: c.data == "top")
-async def inline_top(callback_query: types.CallbackQuery):
+@dp.callback_query_handler(lambda c: c.data in ["top", "top_rating", "rating"])
+async def rating(callback_query: types.CallbackQuery):
+    """Send top rating to group chat"""
     await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, text=generate_top(3), reply_markup=get_markup())
+    await bot.send_message(callback_query.message.chat.id, text=top_rating(), reply_markup=get_markup())
 
 
-@dp.callback_query_handler(lambda c: c.data == "stats")
-async def inline_personal_info(callback_query: types.CallbackQuery):
+@dp.callback_query_handler(lambda c: c.data in ["personal_info", "personal_statistics", "personal"])
+async def personal_info(callback_query: types.CallbackQuery):
+    """Send personal info, only to private chat"""
     await bot.answer_callback_query(callback_query.id)
     alias = '@' + callback_query.from_user.username
-    answer = ""
 
     try:
         player = parse_data(import_string())[alias]
--- users_info.py
+++ users_info.py
@@ -1,8 +1,8 @@
 from pyrebase import initialize_app
-import dotenv
+from dotenv import load_dotenv, find_dotenv
 from os import getenv
 
-dotenv.load_dotenv(dotenv.find_dotenv())
+load_dotenv(find_dotenv())
 
 DATA: dict = {
     "apiKey": getenv("apiKey"),
@@ -17,10 +17,12 @@ DATA: dict = {
 
 
 def import_string() -> str:
+    """import STRING from data"""
     return initialize_app(DATA).database().child("save").get().val()
 
 
 def parse_data(string: str = "") -> dict:
+    """parsing STRING to normal list"""
     players: dict[dict] = {}
 
     substring = string.split(";")
@@ -32,5 +34,68 @@ def parse_data(string: str = "") -> dict:
     return players
 
 
+def generate_top(top_: list) -> list:
+    """generate list with all players, had the same points in top 3 or higher"""
+    top_players: list = [[top_[0]]]
+    index: int = 0
+
+    for i in range(1, len(top_)):
+        if top_[i]['points'] == top_[i - 1]['points']:
+            top_players[index].append(top_[i])
+        else:
+            players = 0
+            for top_player in top_players:
+                players += len(top_player)
+            if players >= 3:
+                break
+            top_players.append([top_[i]])
+            index += 1
+
+    return top_players
+
+
+def top_rating() -> str:
+    """return list of top 3 or higher players (if they have the same points)"""
+    answer = "Top mafia players:\n"
+    top: list = sorted(parse_data(import_string()).values(), key=lambda p: p['points'], reverse=True)
+
+    if top[0] == 0:
+        return "Everybody has the same number of points - 0."
+
+    top = generate_top(top)
+
+    if len(top) == 1:  # All top 3 players have the same number of points
+        answer += f"1-{len(top[0])}: "
+        for player in top[0]:
+            answer += f"{player['name']}, "
+        answer += f"all have {top[0][0]['points']} points."
+    elif len(top) == 2:  # There are 2 groups of players, having 1-2 and 3-... places in rating
+        if len(top[0]) != 1:
+            answer += f"1-{len(top[0])}: "
+            for player in top[0]:
+                answer += f"{player['name']}, "
+            answer += f"all have {top[0][0]['points']} points;\n"
+        else:
+            answer += f"1: {top[0][0]['name']}, {top[0][0]['points']} points;\n"
+
+        answer += f"{len(top[0]) + 1}-{len(top[1]) + len(top[0])}: "
+        for player in top[1]:
+            answer += f"{player['name']}, "
+        answer += f"all have {top[1][0]['points']} points."
+    else:  # 1 and 2 places have different rating and 3-... have another
+        answer += f"1: {top[0][0]['name']}, {top[0][0]['points']} points;\n"
+        answer += f"2: {top[1][0]['name']}, {top[1][0]['points']} points;\n"
+
+        if len(top[2]) != 1:
+            answer += f"3-{len(top[2]) + 2}: "
+            for player in top[2]:
+                answer += f"{player['name']}, "
+            answer += f"all have {top[2][0]['points']} points."
+        else:
+            answer += f"3: {top[2][0]['name']}, {top[2][0]['points']} points."
+
+    return answer
+
+
 if __name__ == "__main__":
-    print(parse_data(import_string()))
+    print(top_rating())
